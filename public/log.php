515d5e12ad8b6d3d689b8229e279116ad65e4612
RTurquin <romeo.turquin@etudiant.univ-reims.fr>
2025-02-09 10:24:19 +0100

    test d'une requete sql en php pour mobile

--- public/log.php
+++ public/log.php
@@ -5,23 +5,32 @@ require_once '../database/credentials.php';
 
 // Login API to
 if(isset($_POST['login']) && isset($_POST['password'])){
-     
+
+    $postLogin = mysql_real_escape_string($_POST['login']);
+    $postPassword = mysql_real_ecape_string($_POST['password']);
+
     echo json_encode($_POST['password'] . "     " . $_POST['login']);
     try{
         $conn = new PDO("mysql:host=$servername;dbname=$database",$dblogin,$password);
+
+
+        $sql = spritf("SELECT count(*) as number FROM users WHERE name LIKE '%s' AND password LIKE '%s'",
+            $postLogin,
+            password_hash($postPassword)
+        );
+
+
+        $result = $conn->query($sql);
+
+        echo json_encode(print_r($result));
+
+
     }catch(PDOException $pe){
-	echo json_encode("Erreur de connexion à la base de donnée : " . $pe->getMessage()) ;
-	http_response_code(500);
-	die();
+	    echo json_encode("Erreur de connexion à la base de donnée : " . $pe->getMessage()) ;
+	    http_response_code(500);
+	    die();
 
     }
-
-
-
-
-
-
-
 
 
 
