505ad5bc725183b0772f72cd07e5b7bba9a4c020
RTurquin <romeo.turquin@etudiant.univ-reims.fr>
2025-02-12 22:40:07 +0100

    test hashing

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -40,7 +40,7 @@ class ApiAuthController extends Controller {
 
             }
 
-            $out =['error' => 1000,'data' => $user[0]->password];
+            $out =['error' => 1000,'data' => $pass];
 
             
         }else if($request->filled('token')){
