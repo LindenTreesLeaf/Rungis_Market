a46ea48c243b476f523f799044a20aada1a00d90
RTurquin <romeo.turquin@etudiant.univ-reims.fr>
2025-02-12 13:15:39 +0100

    ajout du namespace Request pour corriger une erreur

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
