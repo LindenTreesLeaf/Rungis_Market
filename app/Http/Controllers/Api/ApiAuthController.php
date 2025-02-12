1ed3d25b307d306ee201d501b53178796f0a985f
RTurquin <romeo.turquin@etudiant.univ-reims.fr>
2025-02-12 22:54:31 +0100

    PETIT oublie d\'un "ELSE" qui fait que ça marche pas ...

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -40,8 +40,6 @@ class ApiAuthController extends Controller {
 
             }
 
-            $out =['error' => 1000,'data' => count($user),'data2' => Hash::check($pass,$user[0]->password)];
-
             
         }else if($request->filled('token')){
             
