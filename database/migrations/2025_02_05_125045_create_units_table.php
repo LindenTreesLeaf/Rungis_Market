93c74a02c5ddc3ab61e0d6bb5a64de4e54a6f5d4
RTurquin <romeo.turquin@etudiant.univ-reims.fr>
2025-02-19 16:14:44 +0100

    rennomage de name en name_u pour les units pour éviter d'écraser une valeur lors d'une jointure

--- database/migrations/2025_02_05_125045_create_units_table.php
+++ database/migrations/2025_02_05_125045_create_units_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('units', function (Blueprint $table) {
             $table->id();
-            $table->string("name", length:10);
+            $table->string("name_u", length:10);
         });
     }
 
